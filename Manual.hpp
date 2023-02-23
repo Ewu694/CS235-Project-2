bdd54131036decf8d07532de6e4f9b7300a25db3
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-02-23 14:04:28 -0500

    Update Manual.hpp

--- Manual.hpp
+++ Manual.hpp
@@ -1,92 +1,80 @@
-#ifndef MANUAL_HPP_
-#define MANUAL_HPP_
-
-#include <iostream>
-#include <string>
 #include "Book.hpp"
+#include "Novel.hpp"
+#include <string>
 
-class Manual: public Book
+Novel::Novel(std::string title, std::string author, int page_count, std::string novel_genre, bool is_digital, bool novel_film_adaptation):Book(title, author, page_count, is_digital), novel_genre_{novel_genre}, novel_film_adaptation_{novel_film_adaptation}
 {
-    private:
-    std::string manual_url_;
-    std::string device_name_;
-    bool manual_visual_aid_;
-    bool manual_website_;
+}
 
-    public:
-    /**
-    Default constructor.
-    Default-initializes all private members.
-    */
-    Manual() = default;
+std::string Novel::getGenre() const
+{
+    return novel_genre_;
+}
 
-    /**
-     Parameterized constructor.
-    @param     : The title of the book (a string)
-    @param     : The author of the book (a string)
-    @param     : The number of pages in the book (a positive integer)
-    @param     : A flag indicating whether the book is in digital form
-                with DEFAULT VALUE False (a Boolean)
-    @param     : The name of the device (a string)
-    @param     : The URL that is in the format
-                'https://www.something.something'
-                or 'http://www.something.something'
-                with the last 'something' being at least 2 characters
-                with DEFAULT VALUE empty string (a string)
-    @param     : A Boolean indicating the presence of a visual aid
-                with DEFAULT VALUE False
-    @post      : The private members are set to the values of the
-                corresponding parameters.
-                If a URL is provided, the website flag is set to True.
-                If the URL is ill-formatted, the website is set to
-                empty string and the website flag is set to False.
-    */
-    Manual(std::string title, std::string author, int page_count, std::string device_name, bool is_digital = false, std::string url = "", bool visual_aid = false);
+void Novel::setGenre(const std::string &novel_genre)
+{
+    novel_genre_ = novel_genre;
+}
 
-    /**
-    @param  : a reference to a string representing the device
-    @post   : sets the private member variable to the value of the parameter
-    **/
-    void setDevice(const std::string &new_device);
+std::vector<std::string> Novel::getCharacterList() const
+{
+    return novel_characters_;
+}
 
-    /**
-    @return  : the variable indicating the device the manual is for
-    **/
-    std::string getDevice() const;
-    
-    /**
-    @param   : a reference to a website link (string) that is in the format
-             'https://www.something.something'
-             or 'http://www.something.something'
-             with the last 'something' being at least 2 characters
-    @return  : has_website_ flag (a boolean)
-    @post    : If the link is not correctly formatted,
-             store "Broken Link" in the link member variable (see <regex>)
-             and either way set the has website flag to True
-    **/
-   bool setWebsite(const std::string &website);
+std::string Novel::getCharacterListString() const
+{
+    std::string character_list = "";
+    for(int i = 0; i < novel_characters_.size(); i++)
+    {
+            character_list += novel_characters_[i];
+            character_list += " ";
+    }
+    return character_list;
+}
 
-   /**
-   @return  : the url for the website
-   **/
-   std::string getWebsite() const;
+void Novel::addCharacter(const std::string &novel_character)
+{
+    novel_characters_.insert(novel_characters_.begin() + 1, novel_character);
+}
 
-   /**
-   @param  : a boolean
-   @post   : sets the private member variable indicating the presence
-            of a visual aid to the value of the parameter
-   **/
-   void setVisualAid(const bool &visual_aid);
+bool Novel::hasFilmAdaptation() const
+{
+    return novel_film_adaptation_;
+}
 
-   /**
-   @return  : the visual aid flag
-   **/
-   bool hasVisualAid() const;
+void Novel::setFilmAdaptation()
+{
+    novel_film_adaptation_ = true;
+}
 
-   /**
-   @return  : the has website flag
-   **/
-   bool hasWebsite() const;
-};
+double Novel::getAverageRating() const
+{
+    return novel_average_rating_;
+}
 
-#endif
+review Novel::createReview(const double &novel_score, const std::string &novel_rating)
+{
+    review new_novel_review;
+    new_novel_review.score_ = novel_score;
+    new_novel_review.rating_ = novel_rating;
+    return new_novel_review;
+}
+
+void Novel::addReview(const review &novel_review)
+{
+    novel_reviews_.insert(novel_reviews_.begin() + 1, novel_review);
+}
+
+void Novel::calculateAverageRating()
+{
+    double review_total = 0.0;
+    for(int i = 0; i < novel_reviews_.size(); i++)
+    {
+        if(i == novel_reviews_.size() - 1)
+        {
+            review_total/novel_reviews_.size();
+            break;
+        }
+        review_total += novel_reviews_[i].score_;
+    }
+}
