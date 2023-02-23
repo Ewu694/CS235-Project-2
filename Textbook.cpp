85f9757abec0db8c8545641b6cdf7c0f5e98a3d2
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-02-23 14:05:58 -0500

    Update Textbook.cpp

--- Textbook.cpp
+++ Textbook.cpp
@@ -2,7 +2,7 @@
 #include "Book.hpp"
 #include <string>
 
-Textbook::Textbook(std::string title, std::string author, int page_count, std::string subject, bool is_digital, grade_level textbook_grade_level, bool has_review_questions = false):Book(title, author, page_count, is_digital), textbook_subject_{subject}, textbook_grade_level_{textbook_grade_level}, has_review_questions_{has_review_questions}
+Textbook::Textbook(std::string title, std::string author, int page_count, std::string subject, bool is_digital, grade_level textbook_grade_level, bool has_review_questions):Book(title, author, page_count, is_digital), textbook_subject_{subject}, textbook_grade_level_{textbook_grade_level}, has_review_questions_{has_review_questions}
 {
 }
 
@@ -18,13 +18,16 @@ std::string Textbook::getSubject() const
 
 void Textbook::setGradeLevel(const grade_level &book_grade_level)
 {
-    textbook_grade_level = book_grade_level;
+    textbook_grade_level_ = book_grade_level;
 }
 
 std::string Textbook::getGradeLevel() const
 {
     switch(textbook_grade_level_)
     {
+        case 0:
+            return "NONE";
+            break;
         case 1:
             return "ELEMENTARY";
             break;
